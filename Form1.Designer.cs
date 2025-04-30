a311f20ed6c7c00bd67af5f7ef429376aa20bf95
Ruslan Novikov <dib.08@mail.ru>
2025-05-01 00:06:56 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Exam
+{
+    partial class TicketSystem
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(121, 113);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(114, 31);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(241, 113);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(114, 31);
+            textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(361, 113);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(114, 31);
+            textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(481, 113);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(114, 31);
+            textBox4.TabIndex = 0;
+            // 
+            // TicketSystem
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "TicketSystem";
+            Text = "Ticket System";
+            Load += TicketSystem_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace Exam
+{
+    public partial class TicketSystem : Form
+    {
+        public TicketSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void TicketSystem_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
