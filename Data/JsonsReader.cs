645bf8b6f83a53e6f7aac2796191a2b4ef58c51e
Ruslan Novikov <dib.08@mail.ru>
2025-05-04 00:09:12 +0300

    added class for json read with some functionality

---
+++ Data/JsonsReader.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Data
+{
+    public class JsonsReader
+    {
+        public static string ReadJsonFile(string path)
+        {
+            var config = new ConfigurationBuilder()
+       .SetBasePath(path)
+       .AddJsonFile("appconfig.json")
+       .Build();
+            return config.GetConnectionString("Default");
+        }
+    }
+}
