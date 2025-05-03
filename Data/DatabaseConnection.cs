6bec55348676cfdc1eff7e8e422aafed87f9b2d6
Ruslan Novikov <dib.08@mail.ru>
2025-05-03 23:42:09 +0300

    added class for work with database

---
+++ Data/DatabaseConnection.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Data
+{
+    internal class DatabaseConnection
+    {
+        private string connectionString;
+        private static NpgsqlConnection databaseConnection=new();
+        public DatabaseConnection()
+        {
+        }
+        public void CloseConnection() => databaseConnection.Close();
+        public void OpenConnection() 
+        {
+            try
+            {
+
+                databaseConnection.ConnectionString = connectionString;
+                databaseConnection.Open();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic.ApplicationServices;
-
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using Exam.Data;
 namespace Exam
 {
     public partial class TicketSystem : Form
     {
         public TicketSystem()
         {
+
+            DatabaseConnection connection =new();
+            connection.OpenConnection();
+            MessageBox.Show("works");
+            connection.CloseConnection();
             InitializeComponent();
         }
 
