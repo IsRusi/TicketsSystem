aebeeff7b4850b41031dbfb20909561b47e8da55
Ruslan Novikov <dib.08@mail.ru>
2025-05-04 23:54:29 +0300

    added start menu with search bar

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,61 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
+            textBoxFrom = new TextBox();
+            textBoxWhere = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            searchRouteButton = new Button();
+            labelFrom = new Label();
+            labelWhere = new Label();
+            panelSearchRouteBar = new Panel();
+            panelSearchRouteBar.SuspendLayout();
             SuspendLayout();
             // 
-            // textBox1
+            // textBoxFrom
             // 
-            textBox1.Location = new Point(121, 113);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(114, 31);
-            textBox1.TabIndex = 0;
+            textBoxFrom.Location = new Point(5, 28);
+            textBoxFrom.Name = "textBoxFrom";
+            textBoxFrom.PlaceholderText = "Откуда";
+            textBoxFrom.Size = new Size(180, 31);
+            textBoxFrom.TabIndex = 0;
             // 
-            // textBox2
+            // textBoxWhere
             // 
-            textBox2.Location = new Point(241, 113);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(114, 31);
-            textBox2.TabIndex = 0;
+            textBoxWhere.Location = new Point(275, 28);
+            textBoxWhere.Name = "textBoxWhere";
+            textBoxWhere.PlaceholderText = "Куда";
+            textBoxWhere.Size = new Size(180, 31);
+            textBoxWhere.TabIndex = 0;
             // 
-            // textBox3
+            // dateTimePicker1
             // 
-            textBox3.Location = new Point(361, 113);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(114, 31);
-            textBox3.TabIndex = 0;
+            dateTimePicker1.CalendarMonthBackground = SystemColors.MenuBar;
+            dateTimePicker1.Cursor = Cursors.No;
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.ImeMode = ImeMode.NoControl;
+            dateTimePicker1.Location = new Point(509, 26);
+            dateTimePicker1.MaxDate = new DateTime(2025, 5, 4, 0, 0, 0, 0);
+            dateTimePicker1.MinDate = new DateTime(2024, 5, 4, 0, 0, 0, 0);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(126, 31);
+            dateTimePicker1.TabIndex = 1;
+            dateTimePicker1.Value = new DateTime(2025, 5, 4, 0, 0, 0, 0);
+            dateTimePicker1.CloseUp += DateTimePicker1_CloseUp;
+            dateTimePicker1.ChangeUICues += dateTimePicker1_ChangeUICues;
             // 
-            // textBox4
+            // searchRouteButton
             // 
-            textBox4.Location = new Point(481, 113);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(114, 31);
-            textBox4.TabIndex = 0;
+            searchRouteButton.Location = new Point(674, 25);
+            searchRouteButton.Name = "searchRouteButton";
+            searchRouteButton.Size = new Size(112, 34);
+            searchRouteButton.TabIndex = 2;
+            searchRouteButton.Text = "найти";
+            searchRouteButton.UseVisualStyleBackColor = true;
+            // 
+            // labelFrom
+            // 
+            labelFrom.AutoSize = true;
+            labelFrom.Location = new Point(5, 0);
+            labelFrom.Name = "labelFrom";
+            labelFrom.Size = new Size(67, 25);
+            labelFrom.TabIndex = 3;
+            labelFrom.Text = "откуда";
+            // 
+            // labelWhere
+            // 
+            labelWhere.AutoSize = true;
+            labelWhere.Location = new Point(275, 0);
+            labelWhere.Name = "labelWhere";
+            labelWhere.Size = new Size(49, 25);
+            labelWhere.TabIndex = 3;
+            labelWhere.Text = "куда";
+            // 
+            // panelSearchRouteBar
+            // 
+            panelSearchRouteBar.BackColor = SystemColors.ScrollBar;
+            panelSearchRouteBar.BorderStyle = BorderStyle.Fixed3D;
+            panelSearchRouteBar.Controls.Add(labelWhere);
+            panelSearchRouteBar.Controls.Add(labelFrom);
+            panelSearchRouteBar.Controls.Add(searchRouteButton);
+            panelSearchRouteBar.Controls.Add(dateTimePicker1);
+            panelSearchRouteBar.Controls.Add(textBoxWhere);
+            panelSearchRouteBar.Controls.Add(textBoxFrom);
+            panelSearchRouteBar.Location = new Point(44, 90);
+            panelSearchRouteBar.Name = "panelSearchRouteBar";
+            panelSearchRouteBar.Size = new Size(795, 78);
+            panelSearchRouteBar.TabIndex = 4;
             // 
             // TicketSystem
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            ClientSize = new Size(938, 450);
+            Controls.Add(panelSearchRouteBar);
             Name = "TicketSystem";
             Text = "Ticket System";
-            Load += TicketSystem_Load;
+            panelSearchRouteBar.ResumeLayout(false);
+            panelSearchRouteBar.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
+        private TextBox textBoxFrom;
+        private TextBox textBoxWhere;
+        private DateTimePicker dateTimePicker1;
+        private Button searchRouteButton;
+        private Label labelFrom;
+        private Label labelWhere;
+        private Panel panelSearchRouteBar;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -10,12 +10,39 @@ namespace Exam
     {
         public TicketSystem()
         {
+
             InitializeComponent();
+            InitializeMinDateTimePicker();
+            InitializeMaxDateTimePicker();
+            InitializeDateCustomFormat();
         }
 
-        private void TicketSystem_Load(object sender, EventArgs e)
+        private void InitializeMaxDateTimePicker()
         {
+            DateTime nextMonth = DateTime.Today.AddMonths(1);
+            dateTimePicker1.MaxDate = new DateTime(nextMonth.Year, nextMonth.Month,
+                              DateTime.DaysInMonth(nextMonth.Year, nextMonth.Month));
+        }
+        private void InitializeMinDateTimePicker()
+        {
+            DateTime tempDate = new(DateTime.Now.Year, DateTime.Now.Month, 1);
+            dateTimePicker1.MinDate = tempDate;
+        }
+        private void InitializeDateCustomFormat()
+        {
+            dateTimePicker1.CustomFormat = " ";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+        }
+        private void DateTimePicker1_CloseUp(object sender, EventArgs e)
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.CustomFormat = null;
+        }
 
+        private void dateTimePicker1_ChangeUICues(object sender, UICuesEventArgs e)
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.CustomFormat=null;
         }
     }
 }
