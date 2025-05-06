5aee2ac1ed46b166422ae1be5138ce5b6b184bdc
Ruslan Novikov <dib.08@mail.ru>
2025-05-06 22:40:19 +0300

    добавлен метод для получения класса и класс стал универсальней

--- DataAccess/Repositories/DatabaseConnection.cs
+++ DataAccess/Repositories/DatabaseConnection.cs
@@ -1,21 +1,23 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Npgsql;
+using Exam.DataAccess.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exam.Data
+namespace Exam.DataAccess.Repositories
 {
     internal class DatabaseConnection
     {
         private const string path= "O:\\programming\\Exam Project\\C#\\Exam\\keys\\";
         private string connectionString;
-        private static NpgsqlConnection databaseConnection=new();
+        private NpgsqlConnection databaseConnection=new();
         public DatabaseConnection()
         {
             connectionString = JsonsReader.ReadJsonFile(path);
+            databaseConnection.ConnectionString = connectionString;
         }
         public void CloseConnection() => databaseConnection.Close();
         public void OpenConnection() 
@@ -28,9 +30,10 @@ namespace Exam.Data
             }
             catch (Exception ex)
             {
-
+                MessageBox.Show("ошибка подключения : "+ex.Message);
             }
         }
-        
+        public NpgsqlConnection GetConnection() => databaseConnection;
+    
     }
 }
