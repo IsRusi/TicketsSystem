7645edce4f996bcfaa2ded4924da5279f24d2ffb
Ruslan Novikov <dib.08@mail.ru>
2025-05-07 19:42:43 +0300

    refactor(Passanger.cs): изменено имя класса     refactor(UsersRole.cs): изменено поле в классе     refactor(UserRepository.cs): добавлен метод для получения роли пользователя,метод для проверки на существование email

--- DataAccess/Repositories/UserRepository.cs
+++ DataAccess/Repositories/UserRepository.cs
@@ -25,7 +25,7 @@ namespace Exam.DataAccess.Repositories
         {
             dataBaseConnection.OpenConnection();
             commands.Connection = dataBaseConnection.GetConnection();
-            commands.CommandText = "select users.user_id,users.passenger_id,users.password_hash from users inner join passenger on passenger.passenger_id=users.passenger_id where passenger.email = @email";
+            commands.CommandText = "select users.user_id,users.passanger_id,users.password_hash from users inner join passanger on passanger.passanger_id=users.passanger_id where passanger.email = @email";
             commands.Parameters.Clear();
             commands.Parameters.AddWithValue("@email", email);
             User? user = null;
@@ -36,7 +36,7 @@ namespace Exam.DataAccess.Repositories
                     user = new User
                     {
                         Id = Convert.ToInt32(reader["user_id"]),
-                        PassengerId = Convert.ToInt32(reader["passenger_id"]),
+                        PassengerId = Convert.ToInt32(reader["passanger_id"]),
                         PasswordHash = reader["password_hash"].ToString()
                     };
                 }
@@ -44,40 +44,59 @@ namespace Exam.DataAccess.Repositories
             dataBaseConnection.CloseConnection();
         return user;
         }
-        public string GetPasswordByEmail(string email)
+        public UsersRole? GetUserRoleByEmail(string email)
         {
             dataBaseConnection.OpenConnection();
             commands.Connection = dataBaseConnection.GetConnection();
-            commands.CommandText = "select users.password_hash from users inner join passenger on passenger.passenger_id=users.passenger_id where passenger.email = @email";
+            commands.CommandText = @"SELECT u.user_id, r.role_name
+FROM users u
+JOIN usersrole ur ON u.user_id = ur.user_id
+JOIN role r ON ur.role_id = r.role_id
+JOIN passanger p ON p.passanger_id = u.passanger_id
+WHERE p.email = @Email
+";
             commands.Parameters.Clear();
-            commands.Parameters.AddWithValue("@email", email);
-            string password=string.Empty;
+            commands.Parameters.AddWithValue("@Email", email);
+            UsersRole? user = null;
             using (var reader = commands.ExecuteReader())
             {
-                while (reader.Read())
+                if (reader.Read())
                 {
-                    password = reader["password_hash"].ToString();
+                    user = new UsersRole
+                    {
+                        Id = Convert.ToInt32(reader["user_id"]),
+                        RoleName = reader["role_name"].ToString()
+                    };
                 }
             }
+
             dataBaseConnection.CloseConnection();
-            return password;
+            return user;
         }
-        public void AddPassanger(Passenger passenger)
+        public bool ExistsByEmail(string email)
         {
             dataBaseConnection.OpenConnection();
             commands.Connection = dataBaseConnection.GetConnection();
-            commands.CommandText = @"insert into passenger(first_name,second_name,middle_name,email,phone,document_number,gender,birth_date) values(@passenger)";
+            string query = "SELECT COUNT(*) FROM users WHERE email = @email"; 
+            commands.Parameters.AddWithValue("@email", email);
+            return (long)commands.ExecuteScalar() > 0;
+        }
+        public void AddPassanger(Passanger passanger)
+        {
+            dataBaseConnection.OpenConnection();
+            commands.Connection = dataBaseConnection.GetConnection();
+            commands.CommandText = @"insert into passanger(first_name,second_name,middle_name,email,phone,document_number,gender,birth_date) values(@passanger)";
             commands.Parameters.Clear();
-            commands.Parameters.AddWithValue("@passenger", passenger.ToString());
+            commands.Parameters.AddWithValue("@passanger", passanger.ToString());
             dataBaseConnection.CloseConnection();
         }
         public void AddUser(User user)
         {
             dataBaseConnection.OpenConnection();
             commands.Connection = dataBaseConnection.GetConnection();
-            commands.CommandText = @"insert into passenger(first_name,second_name,middle_name,email,phone,document_number,gender,birth_date) values(@passenger)";
+            commands.CommandText = @"insert into users(passenger_id,password_hash) values(@user)";
             commands.Parameters.Clear();
-            commands.Parameters.AddWithValue("@passenger", user.ToString());
+            commands.Parameters.AddWithValue("@user", user.ToString());
             dataBaseConnection.CloseConnection();
         }
     }
