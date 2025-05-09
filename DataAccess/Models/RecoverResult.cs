1923ea39b35debde7ae44c241a906bcf4f5f5d6a
Ruslan Novikov <dib.08@mail.ru>
2025-05-09 13:09:28 +0300

    added(RecoveryPasswordForm.cs): добавлена форма для восстановления аккаунта added(RecoverResult.cs): перечисления для ошибок при восстановлении аккаунта moved(*.cs): enums были перемещены в папку models

---
+++ DataAccess/Models/RecoverResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Logic
+{
+        public enum RecoverResult
+    {
+        Success,
+        InvalidPassword,
+        InvalidEmail,
+        InvalidUser,
+        ChangeFailed
+    }
+}
---
+++ Forms/RecoveryPasswordForm.cs
@@ -0,0 +1,39 @@
+﻿using Exam.DataAccess.Models;
+using Exam.Logic;
+using Exam.Utils;
+using Microsoft.VisualBasic.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exam.Forms
+{
+    public partial class RecoveryPasswordForm : Form
+    {
+        private UserLogic userService = new();
+        public RecoveryPasswordForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNextStepRecoveryAccount_Click(object sender, EventArgs e)
+        { 
+            RecoverResult isChangedPassword = userService.RecoverPassword(textBoxRecoveryEmail.Text, textBoxNewPassword.Text);
+            if (isChangedPassword == RecoverResult.Success)
+            {
+                MessageBox.Show("успешно изменен пароль");
+            }
+            else
+            {
+                MessageBox.Show("пароль не изменен");
+
+            }
+        }
+    }
+}
